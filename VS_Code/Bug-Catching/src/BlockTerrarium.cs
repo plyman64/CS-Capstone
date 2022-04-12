913a10c60b4d9971a587b5b6c4a9783ff09cd628
plyman64 <paigelyman64@gmail.com>
2022-04-12 19:08:30 -0400

    Still working on terrarium

--- VS_Code/Bug-Catching/src/BlockTerrarium.cs
+++ VS_Code/Bug-Catching/src/BlockTerrarium.cs
@@ -4,5 +4,40 @@ namespace BugCatching {
 
     public class BlockTerrarium : Block {
 
+        public bool hasBugInside;
+
+        public BlockTerrarium() {
+            this.hasBugInside = false;
+        }
+        public virtual bool OnBlockInteractStart(float secondsUsed, IWorldAccessor world, IPlayer byPlayer, BlockSelection blockSel) {
+
+            //If there is already a bug in the terrarium
+            if(!hasBugInside) {
+                api.Logger.Debug("Bug already in terrarium!");
+                return false;
+            }
+
+            ItemStack currentItemStack = new ItemStack();
+            currentItemStack = byPlayer.InventoryManager.ActiveHotbarSlot.Itemstack;
+
+            api.Logger.Debug("currentItemStack.Item.FirstCodePart: " + currentItemStack.Item.FirstCodePart());
+            bool holdingBug = (currentItemStack.Item.FirstCodePart() == "beetle");
+
+            if(holdingBug) {
+                api.Logger.Debug("Spawning bug in terrarium!");
+
+                EntityBug bug = new EntityBug();
+                bug.TeleportTo(blockSel.Position);
+
+                hasBugInside = true;
+
+                currentItemStack.StackSize--;
+            }
+
+            base.OnBlockInteractStop(secondsUsed, world, byPlayer, blockSel);
+
+            return true;
+        }
+
     }
 }
